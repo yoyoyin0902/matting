6965bd4bf10e95476b596374e1afc0929d25f410
yoyoyin0902 <amy0937968@gmail.com>
2022-01-19 11:23:28 +0800

    0118

--- img_PostProcessing.py
+++ img_PostProcessing.py
@@ -1,26 +1,87 @@
 import cv2
 import numpy as np
 import skimage.morphology as sm
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from skimage import io,color
 
 savefile_path = "/home/user/matting/preprocessingfile/"
+# savefile_path = "/home/user/matting/preprocessingfile/com/"
 
-def opening(image_path):
-    img = cv2.imread(image_path)
-    # cv2.imshow('old',img)
-    cv2.imwrite(savefile_path+"old.jpg",img)
+def opening(image_path,com_path):
+    img = cv2.imread(image_path,0)
+    com_img = cv2.imread(com_path,0)
+    #cv2.imshow('origin',img)
+    
+    # laplacian = cv2.Laplacian(img,cv2.CV_64F)
+    # sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=7)
+    # sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=7)
+
+    # abs_sobelx = np.absolute(sobelx)
+    # scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+    # thresh_min = 10
+    # thresh_max = 170
+    # sxbinary = np.zeros_like(scaled_sobel)
+    # sxbinary[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
+    # plt.imshow(sxbinary, cmap='gray') 
+    # plt.show()  
+
+    # plt.subplot(2,2,1),plt.imshow(img,cmap='jet')
+    # plt.title('Original'), plt.xticks([]), plt.yticks([])
+    # plt.subplot(2,2,2),plt.imshow(laplacian,cmap='jet')
+    # plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+    # plt.subplot(2,2,3),plt.imshow(sobelx,cmap='jet')
+    # plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+    # plt.subplot(2,2,4),plt.imshow(sobely,cmap='jet')
+    # plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+    # plt.show()
+    # plt.imshow(img,cmap='jet')
+    # plt.show()
+    # cv2.imwrite(savefile_path+"old1.png",img)
+
+    # kernel = np.ones((20,20),np.uint8)
+    # gradient = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel)
+    # cv2.imshow('erosion',img)
+
+    # gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # plt.imshow(gray_img)
+    # plt.show()
+    #cv2.imshow('gray',gray_img)
+
     
 
-    kernel_erode= np.ones((5,5),np.uint8)
-    kernel_dilation= np.ones((5,5),np.uint8)
+    kernel_erode= np.ones((5,5),np.uint8) #侵蝕
+    kernel_dilation= np.ones((5,5),np.uint8) #膨脹
 
-    img = cv2.erode(img,kernel_erode,iterations = 5)
-    img = cv2.dilate(img,kernel_dilation,iterations = 5)
-    cv2.imwrite(savefile_path+"dilate.jpg",img)
+    img = cv2.erode(img,kernel_erode,iterations =5)
+    img = cv2.dilate(img,kernel_dilation,iterations = 6)
+    cv2.imshow('kernel_dilation',img)
+    # plt.imshow(img)
+    # plt.show()
 
-    ret,binary=cv2.threshold(img,140,255,cv2.THRESH_BINARY)
+    
+    #plt.show()
+
+    #cv2.imshow("erode",img)
+    #cv2.imwrite(savefile_path+"dilate.jpg",img)
+
+    ret,binary=cv2.threshold(img,140,255,cv2.THRESH_BINARY) #二值化
+    
+    # contours, hierarchy = cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)  
+    # img = cv2.drawContours(img.copy(),contours,1,(0,0,255),3) 
     cv2.imshow('binary',binary)
-    cv2.imwrite(savefile_path+"binary.jpg",binary)
+
+    ret, thresh = cv2.threshold(binary, 127, 255, 0)
+    #contours,hierarchy = cv2.threshold(binary, 127, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #cv2.imshow("bitwise_not",hierarchy)
+    aa=cv2.drawContours(binary, contours, -1, (0,255,255), 3)
+    # lunkuo = cv2.bitwise_or(img,hierarchy) #设置80为低阈值，255为高阈值 #輪廓
+    cv2.imshow("bitwise_not",aa)
+    
+
+
+    #cv2.imwrite(savefile_path+"binary.jpg",binary)
 
     # cv2.imshow("img",img)
 
@@ -30,33 +91,17 @@ def opening(image_path):
     # cv2.imshow('erosion',erosion)
     # ret,binary=cv2.threshold(img,140,255,cv2.THRESH_BINARY)
     # cv2.imshow('binary',binary)
-    count = 30
    
-    
-
-    # kernel = np.ones((30,30), np.uint8)
-
-    # for i in range(count):
-    #     result = cv2.morphologyEx(binary, cv2.MORPH_OPEN, kernel)
-    #     i+=1
-    
-    # for i in range(20):
-    #     result = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel)
-    #     i+=1
-    
-
-    # cv2.imshow("img",img)
-    # cv2.imshow("result", result)
-
-    
-    
-    # cv2.imwrite(savefile_path+"new.jpg",result)
-
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+# def Canny(image_path):
+
 
 
 
 if __name__ == '__main__':
     img_path = "/home/user/matting/img_output/pha/pha2.jpg"
-    opening(img_path)
+    com_path = "/home/user/matting/img_output/com/com2.png"
+    #opening(img_path)
+    opening(img_path,com_path)
---
+++ realtime_shape_detection.py
@@ -0,0 +1,100 @@
+import cv2
+import numpy as np
+from PIL import Image
+
+savefile_path = "/home/user/matting/preprocessingfile/"
+
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+
+def empty(a):
+    pass
+
+cv2.namedWindow("Parameters")
+cv2.resizeWindow("Parameters",300,300)
+cv2.createTrackbar("Threshold1","Parameters",23,255,empty)
+cv2.createTrackbar("Threshold2","Parameters",20,255,empty)
+cv2.createTrackbar("Area","Parameters",5000,30000,empty)
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+def getContours(img,imgContour):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    #print(contours)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        areaMin = cv2.getTrackbarPos("Area", "Parameters")
+        if area > areaMin:
+            cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 5)
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+            print(len(approx))
+            x , y , w, h = cv2.boundingRect(approx)
+            cv2.rectangle(imgContour, (x , y ), (x + w , y + h ), (0, 255, 0), 5)
+
+            cv2.putText(imgContour, "Points: " + str(len(approx)), (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, .7,
+                        (0, 255, 0), 2)
+            cv2.putText(imgContour, "Area: " + str(int(area)), (x + w + 20, y + 45), cv2.FONT_HERSHEY_COMPLEX, 0.7,
+                        (0, 255, 0), 2)
+
+while True:
+    img_path = "/home/user/matting/img_output/pha/pha2.jpg"
+    img = cv2.imread(img_path)
+
+    #success, img = cap.read()
+    imgContour = img.copy()
+
+
+    imgBlur = cv2.GaussianBlur(img, (11, 11), 20)
+    imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
+
+    threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
+    threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
+    ret,binary=cv2.threshold(imgGray,140,255,cv2.THRESH_BINARY) #二值化
+    
+    imgCanny = cv2.Canny(binary,threshold1,threshold2)
+    
+
+    kernel = np.ones((5, 5),np.uint8)
+    imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
+
+    getContours(imgDil,imgContour)
+    # imgStack = stackImages(0.8,([img,imgCanny],[imgDil,imgContour]))
+    imgStack = stackImages(0.8,([imgContour,binary],[imgDil,imgGray]))
+    cv2.imwrite(savefile_path+"imgStack.jpg",imgStack)
+    cv2.imshow("Result", imgStack)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
