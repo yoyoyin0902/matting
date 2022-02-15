3d927ad90d71ce5cda675fe65250cf9fb80c5ffd
yoyoyin0902 <amy0937968@gmail.com>
2022-02-15 18:07:44 +0800

    0215

--- area_calculate/turn.py
+++ area_calculate/turn.py
@@ -3,6 +3,31 @@ import numpy as np
 import matplotlib.pylab as plt
 import time
 
+
+def homography(Original_imgPath):
+  Orig_img = cv2.imread(Original_imgPath)
+  row, cols = Orig_img.shape[:2]
+
+  #灰階處理
+  gray = cv2.cvtColor(Orig_img,cv2.COLOR_BGR2GRAY)
+  #二值化
+  ret,binary = cv2.threshold(gray,100,255,cv2.THRESH_BINARY)
+  #找尋輪廓並繪製輪廓(二值化過後才可用)
+  contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+  cv2.drawContours(img,contours,-1,(0,0,255),3) 
+# cv2.imshow('binary2',img)
+
+  #尋找輪廓中心點並繪製中心點
+  M = cv2.moments(contours[0])
+  cx = int(M['m10']/M['m00'])
+  cy = int(M['m01']/M['m00'])
+  cv2.circle(img, (cx,cy), 2,(255,0,0),2)
+
+  cv2.imshow('get_center',img)
+
+  
+
+
 #15483951094941.jpg
 img=cv2.imread('1928832.jpg')
 
@@ -13,29 +38,47 @@ rows, cols = img.shape[:2] #row 867 col 1680
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #二值化
 ret, binary = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+
+newImage = binary.copy()
 #找尋輪廓(二值化過後才可用)
 contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
 #繪製輪廓()
-#cv2.drawContours(img,contours,-1,(0,0,255),3) 
+cv2.drawContours(img,contours,-1,(0,0,255),3) 
 # cv2.imshow('binary2',img)
+
+# M = cv2.moments(contours[0])
+# cx = int(M['m10']/M['m00'])
+# cy = int(M['m01']/M['m00'])
+# print(cx)
+# print(cy)
 
 print("num of contours: {}".format(len(contours)))
 
 #得到最小外接矩形的（中心(x,y), (宽,高), 旋转角度）
-((cx, cy), (width, height), theta) = cv2.minAreaRect(contours[0])  #获取蓝色矩形的中心点、宽高、角度
+rect = cv2.minAreaRect(contours[0])  #获取蓝色矩形的中心点、宽高、角度
 
-# if theta < -45 or theta > 45:
-# 	rect = ((cx, cy), (width, height), theta - 90)
-# else :
-rect = ((cx, cy), (width, height), theta)
-angle = theta
-width = int(width)
-height = int(height)
-print(width)
-print(height)
-print(cx)
-print(cy)
+angle = rect[-1]
+width = int(rect[1][0])
+height = int(rect[1][1])
+cx = rect[0][0]
+cy = rect[0][1]
 print(angle)
+
+# cv2.circle(img, (int(cx),int(cy)), 2,(255,0,0),2)
+
+# cv2.imshow('circle',img)
+
+# if rect[-1] < -45 or rect[-1] > 45:
+# 	rect = (rect[0], (rect[1][0], rect[1][1]), rect[2] - 90)
+# else :
+#   rect = (rect[0], (rect[1][0], rect[1][1]), rect[2])
+
+# print(angle)
+# print(width)
+# print(height)
+# print(cx)
+# print(cy)
+# print(angle)
 
 #矩形四個頂點
 box = cv2.boxPoints(rect) 
@@ -44,7 +87,7 @@ box = cv2.boxPoints(rect)
 boxlist = np.int0(box) #numpy.int64
 # print(boxlist)
 cv2.drawContours(img, [boxlist], 0, (0, 255, 0), 1)
-cv2.imshow('box',img)
+# cv2.imshow('box',img)
 
 # x,y,w,h = cv2.boundingRect(rect) #（x,y）是旋转的边界矩形左上角的点，w ,h分别是宽和高
 # print(x)
@@ -86,9 +129,10 @@ warped = cv2.warpPerspective(img, M, (cols, rows))
 
 if angle<=-90:  #对-90度以上图片的竖直结果转正
     warped = cv2.transpose(warped)
-    warped = cv2.flip(warped, 0)  # 逆时针转90度，如果想顺时针，则0改为1
+    warped = cv2.flip(warped, 1)  # 逆时针转90度，如果想顺时针，则0改为1
     warped=warped.transpose
-cv2.imwrite("save2.jpg",warped)
+cv2.imwrite("save3.jpg",warped)
 cv2.imshow('wr1',warped)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
